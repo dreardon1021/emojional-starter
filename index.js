904060c0830b1579bd158b5bbf8333602da3503e
Dan Reardon <dreardon1021@gmail.com>
2019-12-06 11:23:37 -0700

    ...

--- index.js
+++ index.js
@@ -4,27 +4,12 @@ var happyButton = document.querySelector('.happy');
 var message = document.querySelector('.message');
 
 
-
-
-var numbers = [0, 1 ,2]
-function shuffle(o) {
-    for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-    return o;
-};
-
-var random = shuffle(numbers)
-
-
-
-
-
-
-
 sadButton.addEventListener ('click', addSadMessage);
 
-function addSadMessage (shuffle(o)){
+function addSadMessage (){
   console.log ('addSadMessage')
-  if (randomNumber === 0){
+  var random = Math.floor(Math.random() * 3);
+  if (random === 0){
       message.innerText = 'Do you want to talk?';
     } else if (random === 1) {
       message.innerText = 'Keep your head up';
@@ -40,7 +25,8 @@ function addSadMessage (shuffle(o)){
 sillyButton.addEventListener ('click', addSillyMessage);
 
 function addSillyMessage (){
-  if (randomNumber === 0){
+  var random = Math.floor(Math.random() * 3);
+  if (random === 0){
       message.innerText = 'Interesting response';
     } else if (random === 1) {
       message.innerText = 'Sounds....good?';
@@ -54,12 +40,13 @@ function addSillyMessage (){
 happyButton.addEventListener ('click', addHappyMessage);
 
 function addHappyMessage (){
-  if (randomNumber === 0){
+  var random = Math.floor(Math.random() * 3);
+  if (random === 0){
       message.innerText = 'You go!';
     } else if (random === 1) {
-      message.innerText = 'Your smile just cheered me up!';
+      message.innerText = 'happy?';
     } else if (random === 2){
-      message.innerText = 'Yay!';
+      message.innerText = 'Me too';
     } else {
       message.innerText = 'ERROR!!';
   };
