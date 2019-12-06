510439eb1a9a515d3092a2583c2410535478e71d
Dan Reardon <dreardon1021@gmail.com>
2019-12-06 12:01:13 -0700

    attempt

--- index.js
+++ index.js
@@ -4,16 +4,28 @@ var happyButton = document.querySelector('.happy');
 var message = document.querySelector('.message');
 
 
+  var possibleNums = [0, 1, 2];
+
+  generate = function random() {
+    for(i = 0; i < possibleNums.length; i++) {
+    console.log(possibleNums[i]);
+    return possibleNums[i];
+  };
+};
+
+
+
+
 sadButton.addEventListener ('click', addSadMessage);
 
 function addSadMessage (){
   console.log ('addSadMessage')
-  var random = Math.floor(Math.random() * 3);
-  if (random === 0){
+  // var random = Math.floor(Math.random() * 3);
+  if (generate() === 0){
       message.innerText = 'Do you want to talk?';
-    } else if (random === 1) {
+    } else if (generate()  === 1) {
       message.innerText = 'Keep your head up';
-    } else if (random === 2){
+    } else if (generate()  === 2){
       message.innerText = 'What can I do?';
     } else {
       message.innerText = 'ERROR!!';
@@ -25,12 +37,12 @@ function addSadMessage (){
 sillyButton.addEventListener ('click', addSillyMessage);
 
 function addSillyMessage (){
-  var random = Math.floor(Math.random() * 3);
-  if (random === 0){
+  // var random = Math.floor(Math.random() * 3);
+  if (generate() === 0){
       message.innerText = 'Interesting response';
-    } else if (random === 1) {
+    } else if (generate()  === 1) {
       message.innerText = 'Sounds....good?';
-    } else if (random === 2){
+    } else if (generate()  === 2){
       message.innerText = 'Me too';
     } else {
       message.innerText = 'ERROR!!';
@@ -40,12 +52,12 @@ function addSillyMessage (){
 happyButton.addEventListener ('click', addHappyMessage);
 
 function addHappyMessage (){
-  var random = Math.floor(Math.random() * 3);
-  if (random === 0){
+  // var random = Math.floor(Math.random() * 3);
+  if (generate() === 0){
       message.innerText = 'You go!';
-    } else if (random === 1) {
+    } else if (generate()  === 1) {
       message.innerText = 'happy?';
-    } else if (random === 2){
+    } else if (generate()  === 2){
       message.innerText = 'Me too';
     } else {
       message.innerText = 'ERROR!!';
