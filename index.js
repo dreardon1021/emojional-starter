30d902ba7ab0d77df6585139a2bf3292a0fb2e6b
Dan Reardon <dreardon1021@gmail.com>
2019-12-06 10:12:00 -0700

    merged branches because I cant merge without writer access

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
+var sadButton = document.querySelector('.crying');
+var sillyButton = document.querySelector('.silly');
+var happyButton = document.querySelector('.happy');
+var message = document.querySelector('.message');
 
+
+sadButton.addEventListener ('click', addSadMessage);
+
+function addSadMessage (){
+  console.log ('addSadMessage')
+  var random = Math.floor(Math.random() * 3);
+  if (random === 0){
+      message.innerText = 'Do you want to talk?';
+    } else if (random === 1) {
+      message.innerText = 'Keep your head up';
+    } else if (random === 2){
+      message.innerText = 'What can I do?';
+    } else {
+      message.innerText = 'ERROR!!';
+  };
+};
+
+
+
+sillyButton.addEventListener ('click', addSillyMessage);
+
+function addSillyMessage (){
+  var random = Math.floor(Math.random() * 3);
+  if (random === 0){
+      message.innerText = 'Interesting response';
+    } else if (random === 1) {
+      message.innerText = 'Sounds....good?';
+    } else if (random === 2){
+      message.innerText = 'Me too';
+    } else {
+      message.innerText = 'ERROR!!';
+  };
+};
+
+happyButton.addEventListener ('click', addHappyMessage);
+
+function addHappyMessage (){
+  var random = Math.floor(Math.random() * 3);
+  if (random === 0){
+      message.innerText = 'You go!';
+    } else if (random === 1) {
+      message.innerText = 'Your smile just cheered me up!';
+    } else if (random === 2){
+      message.innerText = 'Yay!';
+    } else {
+      message.innerText = 'ERROR!!';
+  };
+};
